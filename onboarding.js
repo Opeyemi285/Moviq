1a5582b995a3de82c8bbbf1524aa7134d431e14d
Abdulkabir Abimbola <abimbolaabdulkabir222@gmail.com>
2025-10-04 23:22:41 +0100

    Refactor button class handling and include onboarding CSS in sign-up page

--- onboarding.js
+++ onboarding.js
@@ -1,6 +1,13 @@
 const nextBtn = document.getElementById('nextbtn');
 const firstDiv = document.querySelector('.first');
 const secondDiv = document.querySelector('.second');
+const genreBtn = document.querySelectorAll('button');
+
+genreBtn.forEach(button => {
+    button.addEventListener('click', () => {
+        button.classList.toggle('clicked');
+    });
+});
 
 nextBtn.addEventListener('click', () => {
     firstDiv.style.display = 'none';
