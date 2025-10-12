a6cf94ca888ec78d454b0b27482b3cd512e4ca11
Abdulkabir Abimbola <abimbolaabdulkabir222@gmail.com>
2025-10-12 04:18:45 +0100

    Refactor movie sections in HTML and CSS for improved structure and styling consistency

--- index.js
+++ index.js
@@ -4,9 +4,12 @@ const TMDB_API_KEY = '960a5f1a965366de5f1696d8f95457c0';
 const BASE_URL = 'https://api.themoviedb.org/3';
 const image_url = 'https://image.tmdb.org/t/p/original';
 const pages = 1
+const main = document.getElementsByName('main');
+const search = document.getElementById('search');
 const container = document.querySelector('.banner')
 const popular = document.querySelector('.popular-movies')
 const trending = document.querySelector('.trending-movies')
+const top_rated = document.querySelector('.top-rated-movies')
 
 async function getPopularMovies() {
     // Construct the URL, appending the api_key as a query parameter
@@ -21,34 +24,21 @@ async function getPopularMovies() {
         }
         
         const data = await response.json();
-        console.log("Popular Movies Data:", data.results);
         if (data.results.length > 0) {
-            // console.log("First movie title:", data.results[0].title);
-            // data.results.forEach(movie => {
-            //     const movieElement = document.createElement('div');
-            //     movieElement.classList.add('movie');
-            //     movieElement.innerHTML = `
-            //         <img src="${image_url + movie.poster_path}" alt="${movie.title}">
-            //         <h3>${movie.title}</h3>
-            //         <p>Rating: ${movie.vote_average}</p>
-            //     `;
-            //     container.appendChild(movieElement);
-            // });
             container.innerHTML = `
                 <img class="banner-img" src="${image_url + data.results[0].backdrop_path}" alt="${data.results[0].title}" style="">
                 <div class="banner-texts">
                     <h3>${data.results[0].title}</h3>
-                    <p style="color: gold; font-size:30px;"> <i class="bi bi-star-fill"></i> ${(data.results[0].vote_average).toFixed(1)} <span style="font-size: 16px;">/10</span></p>
+                    <p class="movie-ratings" style="font-size: 30px;"> <i class="bi bi-star-fill"></i> ${(data.results[0].vote_average).toFixed(1)} <span style="font-size: 16px;">/10</span></p>
                 </div>
             `;
             popular.innerHTML += data.results
             .map(movie => `
                 <div class="movie" id="${movie.id}">
-                    <img src="${image_url + movie.poster_path}" 
-                        alt="${movie.title || movie.name}" loading="lazy"
-                        style="width: 150px; height: 225px; border-radius: 10px; margin-right: 10px;">
-                    <p font-size: 14px;">${movie.title || movie.name}</p>
-                    <p style="font-size: 12px; color: gold;"> <i class="bi bi-star-fill"></i> ${(movie.vote_average).toFixed(1)} <span style="font-size: 10px;">/10</span></p>
+                    <img class="movie-cover" src="${image_url + movie.poster_path}" 
+                        alt="${movie.title || movie.name}" loading="lazy">
+                    <p class="movie-title">${movie.title || movie.name}</p>
+                    <p class="movie-ratings"> <i class="bi bi-star-fill"></i> ${(movie.vote_average).toFixed(1)} <span style="font-size: 10px;">/10</span></p>
                 </div>
             `)
             .join('');
@@ -71,27 +61,15 @@ async function TrendingMovies() {
     }
     
     const data = await response.json();
-    console.log("Trending Movies Data:", data.results);
+    
     if (data.results.length > 0) {
-        console.log("First movie title:", data.results[1].title);
-        // data.results.forEach(movie => {
-        //     const movieElement = document.createElement('div');
-        //     movieElement.classList.add('movie');
-        //     movieElement.innerHTML = `
-        //         <img src="${image_url + movie.poster_path}" alt="${movie.title}">
-        //         <h3>${movie.title}</h3>
-        //         <p>Rating: ${movie.vote_average}</p>
-        //     `;
-        //     container.appendChild(movieElement);
-        // });
         trending.innerHTML += data.results
         .map(movie => `
             <div class="movie" id="${movie.id}">
-                <img src="${image_url + movie.poster_path}" 
-                    alt="${movie.title || movie.name}" loading="lazy"
-                    style="width: 150px; height: 225px; border-radius: 10px; margin-right: 10px;">
-                <p font-size: 14px;">${movie.title || movie.name}</p>
-                <p style="font-size: 12px; color: gold;"> <i class="bi bi-star-fill"></i> ${(movie.vote_average).toFixed(1)} <span style="font-size: 10px;">/10</span></p>
+                <img class="movie-cover" src="${image_url + movie.poster_path}" 
+                    alt="${movie.title || movie.name}" loading="lazy">
+                <p class="movie-title" font-size: 14px;">${movie.title || movie.name}</p>
+                <p class="movie-ratings" style="font-size: 12px; color: gold;"> <i class="bi bi-star-fill"></i> ${(movie.vote_average).toFixed(1)} <span style="font-size: 10px;">/10</span></p>
             </div>
         `)
         .join('');
@@ -101,3 +79,30 @@ async function TrendingMovies() {
     
 }
 TrendingMovies();
+
+async function TopRatedMovies() {
+    const url = `${BASE_URL}/movie/top_rated?api_key=${TMDB_API_KEY}&page=${pages}`;
+
+    const response = await fetch(url);
+    if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    
+    const data = await response.json();
+    if (data.results.length > 0) {
+        top_rated.innerHTML += data.results
+        .map(movie => `
+            <div class="movie" id="${movie.id}">
+                <img class="movie-cover" src="${image_url + movie.poster_path}" 
+                    alt="${movie.title || movie.name}" loading="lazy">
+                <p class="movie-title" font-size: 14px;">${movie.title || movie.name}</p>
+                <p class="movie-ratings" style="font-size: 12px; color: gold;"> <i class="bi bi-star-fill"></i> ${(movie.vote_average).toFixed(1)} <span style="font-size: 10px;">/10</span></p>
+            </div>
+        `)
+        .join('');
+
+    }
+
+    
+}
+TopRatedMovies();
